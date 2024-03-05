b300fa29312b078c6ab3ac6c361e361092ae8057
bruno28araujo <bruno28araujo@gmail.com>
2024-03-05 13:07:39 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-let placar = resultadoPlacar(80, 85)
+let placar = resultadoPlacar(200, 85)
 
 function resultadoPlacar(vitorias, derrotas){
     let ranking = vitorias - derrotas
     return ranking
 }
 
-
-switch(placar){
-    case placar <= 10:
-        console.log("Ferrou! Se esforce mais. Seu resutado foi: " + placar)
-        break
-    case placar >= 11 && placar <= 20:
-        console.log("Parabéns! Você é nível Bronze. Seu resutado foi: " + result)
-        break
-    case placar >= 21 && placar <= 50:
-        console.log("Parabéns! Você é nível Prata. Seu resutado foi: " + result)
-        break
-    case placar >= 51 && placar <= 80:
-        console.log("Parabéns! Você é nível Ouro. Seu resutado foi: " + result)
-        break
-    case placar >= 81 && placar <= 90:
-        console.log("Parabéns! Você é nível Diamante. Seu resutado foi: " + result)
-        break
-    case placar >= 91 && placar <= 100:
-        console.log("Parabéns! Você é Lendário. Seu resutado foi: " + result)
-        break
-    case placar > 101:
-        console.log("Parabéns! Você é imortal. Seu resutado foi: " + result)
-        break
-    default:
-        console.log("Valores errados.")
-        break
-    
+if (placar <= 10){
+    console.log("Ferrou! Se esforce mais.")
+    console.log("Seu resutado foi: " + placar)
+}else if(placar >= 11 && placar <= 20){
+    console.log("Parabéns! Você é nível Bronze.")
+    console.log("Seu resutado foi: " + placar)
+}else if(placar >= 21 && placar <= 50){
+    console.log("Parabéns! Você é nível Prata.")
+    console.log("Seu resutado foi: " + placar)
+}else if(placar >= 51 && placar <= 80){
+    console.log("Parabéns! Você é nível Ouro.")
+    console.log("Seu resutado foi: " + placar)
+}else if(placar >= 81 && placar <= 90){
+    console.log("Parabéns! Você é nível Diamante.")
+    console.log("Seu resutado foi: " + placar)
+}else if(placar >= 91 && placar <= 100){
+    console.log("Parabéns! Você é Lendário.")
+    console.log("Seu resutado foi: " + placar)
+}else if(placar > 101){
+    console.log("Parabéns! Você é imortal.")
+    console.log("Seu resutado foi: " + placar)
+}else{
+    console.log("Valores errados.")
 }
